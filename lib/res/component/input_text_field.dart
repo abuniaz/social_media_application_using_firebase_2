52e3caf6584ae71aaa7f3e8809e3f0a171369c4b
Abu Niaz <niaz35-507@diu.edu.bd>
2023-06-04 23:27:22 +0600

    Update input_text_field.dart

--- lib/res/component/input_text_field.dart
+++ lib/res/component/input_text_field.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
 class InputTextField extends StatelessWidget {
-  const InputTextField({super.key});
+  const InputTextField(
+      {super.key,
+      required this.myController,
+      required this.focusNode,
+      required this.onFieldSubmittedValue,
+      required this.onValidator,
+      required this.keyBoardType,
+      required this.hint,
+      required this.obscureText,
+      required this.enable,
+      required this.autoFocus});
+
+  final TextEditingController myController;
+  final FocusNode focusNode;
+  final FormFieldSetter onFieldSubmittedValue;
+  final FormFieldValidator onValidator;
+  final TextInputType keyBoardType;
+  final String hint;
+  final bool obscureText;
+  final bool enable, autoFocus;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextFormField(
+      controller: myController,
+      focusNode: focusNode,
+      onFieldSubmitted: onFieldSubmittedValue,
+    );
   }
 }
