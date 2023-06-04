29220bdacfb0398c433b92bd72fe63374023cdba
Abu Niaz <niaz35-507@diu.edu.bd>
2023-06-05 01:15:46 +0600

    Update input_text_field.dart

--- lib/res/component/input_text_field.dart
+++ lib/res/component/input_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tech_media/res/color.dart';
 
 class InputTextField extends StatelessWidget {
   const InputTextField(
@@ -31,7 +32,10 @@ class InputTextField extends StatelessWidget {
       onFieldSubmitted: onFieldSubmittedValue,
       validator: onValidator,
       keyboardType: keyBoardType,
-      decoration: InputDecoration(),
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.textFieldDefaultFocus),
+              borderRadius: BorderRadius.all(Radius.circular(15)))),
     );
   }
 }
